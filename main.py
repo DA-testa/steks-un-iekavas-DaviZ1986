f17b1add67edb26d4205e7357bc38e2eb823929e
DaviZ1986 <125762695+DaviZ1986@users.noreply.github.com>
2023-02-19 21:53:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(text[i])
-            indekss = i+1
+            indekss = i
 
         if next in ")]}":
             if len(opening_brackets_stack) == 0:
-                indekss = i+1 
+                indekss = i 
                 return indekss;
             if len(opening_brackets_stack) > 0 and are_matching(opening_brackets_stack[len(opening_brackets_stack)-1], text[i]):
                 opening_brackets_stack.pop(len(opening_brackets_stack)-1)
             elif not are_matching(opening_brackets_stack[len(opening_brackets_stack)-1], text[i]):
-                indekss = i + 1
+                indekss = i
                 return indekss;
     if len(opening_brackets_stack) == 0:
         indekss = 0
