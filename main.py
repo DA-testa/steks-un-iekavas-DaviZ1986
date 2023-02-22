aced8acc7f7983ced9388aebd8caecf48bfdc387
DaviZ1986 <125762695+DaviZ1986@users.noreply.github.com>
2023-02-22 20:28:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,33 +18,21 @@ def find_mismatch(text):
 
         if next in ")]}":
             if len(opening_brackets_stack) == 0:
-                indekss = i 
-                return indekss;
+                return i+1
             if len(opening_brackets_stack) > 0 and are_matching(opening_brackets_stack[len(opening_brackets_stack)-1], text[i]):
                 opening_brackets_stack.pop(len(opening_brackets_stack)-1)
             elif not are_matching(opening_brackets_stack[len(opening_brackets_stack)-1], text[i]):
-                indekss = i
-                return indekss;
-    if len(opening_brackets_stack) == 0:
-        indekss = 0
-    return indekss;
+                return i+1
+    if len(opening_brackets_stack) == 0
+        return "Success"
 
 def main():
     text = input()
-    if (text[0] == "I"):
-        mismatch = find_mismatch(text)
-    elif (text[0] == "F"):
-        f = open("steks-un-iekavas-DaviZ1986/test/", "r")
-        text = f.read()
-        mismatch = find_mismatch(text)
-        f.close()
-
-    if (mismatch == 0):
-        print("Success")
-    else:
-        print(mismatch)
+    mismatch = find_mismatch(text)
+    print(mismatch)
 
 
 
 if __name__ == "__main__":
     main()
+
